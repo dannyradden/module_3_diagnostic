7b407be35d8bc14207189d17c82ffc1df658a005
dannyradden <dradden@gmail.com>
2017-05-17 10:16:15 -0600

    trying to get faraday to work in service. had to temporarly disable webmock and vcr when prying in

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -2,8 +2,11 @@ class NrelService
   attr_reader :connection
 
   def initialize
-    @connection = Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest')
+    @connection = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/")
   end
 
-  
+  def stations(location)
+    require "pry"; binding.pry
+    connection.get('location', location)
+  end
 end
--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe 'NREL Service' do
+  it 'finds stations at location' do
+    @service = NrelService.new
 
+  end
 
 end
