bfa3e061a96374349b8a617652eb955c6d48e075
dannyradden <dradden@gmail.com>
2017-05-17 09:45:24 -0600

    got postman path working. now working on featurte test

--- spec/features/user_can_find_stations.rb
+++ spec/features/user_can_find_stations.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
-describe 'User can find stations' do
-  # As a user
-  # When I visit "/"
+describe 'Dashboard' do
+  it 'User can find nearest stations' do
+    user = User.new(token: ENV["nrel_api_key"])
+
+    visit dashboard_index_path
   # And I fill in the search form with 80203
   # And I click "Locate"
   # Then I should be on page "/search" with parameters visible in the url
   # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
   # And the stations should be limited to Electric and Propane
   # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  end
 end
